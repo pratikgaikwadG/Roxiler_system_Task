c047c3dc9a2276e986eb41aba9d806ce81400d8f
Pratik Gaikwad <132823785+pratikgaikwadG@users.noreply.github.com>
2024-10-08 15:57:29 +0530

    Update Barchart.js

--- frontend/src/components/Barchart.js
+++ frontend/src/components/Barchart.js
@@ -1,10 +1,9 @@
 // BarChartComponent.js
 import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import { Chart, registerables } from "chart.js"; // Import registerables
+import { Chart, registerables } from "chart.js"; 
 import axios from "axios";
 
-// Register all necessary components
 Chart.register(...registerables);
 
 const BarChartComponent = ({ selectedMonth, selectedYear }) => {
@@ -13,37 +12,37 @@ const BarChartComponent = ({ selectedMonth, selectedYear }) => {
   const fetchBarChartData = async () => {
     try {
       const monthIndex =
-        new Date(Date.parse(selectedMonth + " 1, 2020")).getMonth() + 1; // Convert month name to number
+        new Date(Date.parse(selectedMonth + " 1, 2020")).getMonth() + 1; 
       console.log(
         `Fetching data for month: ${monthIndex}, year: ${selectedYear}`
-      ); // Log values
+      ); 
 
       const response = await axios.get(
         `http://localhost:5001/api/transaction/getBarChart?month=${monthIndex}&year=${selectedYear}`
       );
 
       if (response.status === 200) {
-        setChartData(response.data); // Set chart data
+        setChartData(response.data); 
       } else {
         console.error("Failed to fetch data:", response.data);
-        setChartData([]); // Reset data if not successful
+        setChartData([]); 
       }
     } catch (error) {
       console.error(
         "Error fetching bar chart data:",
         error.response ? error.response.data : error.message
       );
-      setChartData([]); // Reset data on error
+      setChartData([]); 
     }
   };
 
   useEffect(() => {
     if (selectedMonth && selectedYear) {
-      fetchBarChartData(); // Fetch data whenever selected month or year changes
+      fetchBarChartData(); 
     }
   }, [selectedMonth, selectedYear]);
 
-  // Prepare data for the Bar chart
+
   const data = {
     labels: [
       "0 - 100",
@@ -60,8 +59,8 @@ const BarChartComponent = ({ selectedMonth, selectedYear }) => {
     datasets: [
       {
         label: "Number of Items",
-        data: chartData.map((range) => range.count || 0), // Extract count from fetched data
-        backgroundColor: "rgba(75, 192, 192, 0.6)", // Example color
+        data: chartData.map((range) => range.count || 0), 
+        backgroundColor: "rgba(75, 192, 192, 0.6)", 
       },
     ],
   };
